43792310cf1e6cf3d783d0dfe8f39614c1b0028b
Ryan Bento <bentopreciliano@gmail.com>
2026-05-19 16:38:47 -0300

    que sonoooooo

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -8,16 +8,29 @@ const Cart = ({ ...props }) => {
   return <div {...props} >
     <div className="flex items-center flex-col w-full h-[250px] ">
      <h1>Sapoato</h1>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjh0nc-PXNOFUmdVVUIrHTJlmiWyMxF9da5g&s" />
-    </div>
-      <h3>
-        O carrinho está <span style={{ "fontWeight": "bold" }}>vazio</span>
-      </h3>
+    
+    
+      {Cart.length === 0 ? (
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjh0nc-PXNOFUmdVVUIrHTJlmiWyMxF9da5g&s" />
+      </div>
+        <h3>
+          O carrinho está <span style={{ "fontWeight": "bold" }}>vazio</span>
+        </h3>    
 
+  
     <div>
       <div onClick={goBack} className="cursor-pointer"><h2>Voltar</h2></div>
       </div>
 
+      ) : ( 
+      
+      <div>
+        {Cart.map(item =>
+        
+          
+
+      
+      }
 
 
    <div className="cursor-pointer"> <footer>Finalizar compra</footer> </div>
