c5ade00970df46a35636d80a40193a4a31a34262
Ryan Bento <bentopreciliano@gmail.com>
2026-05-20 14:04:18 -0300

    20/05/2026

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,49 +1,46 @@
 import { useAppNavigation } from "../hooks/useNavigation"
+import { useStore } from "../hooks/useStore"
 
 const Cart = ({ ...props }) => {
-  const {goBack} = useAppNavigation()  
-  const {goToHome} = useAppNavigation()
+  const { goBack, goToHome } = useAppNavigation()
+  const { cart } = useStore()
 
 
-  return <div {...props} >
+  return (<div {...props} >
     <div className="flex items-center flex-col w-full h-[250px] ">
-     <h1>Sapoato</h1>
-    
-    
-      {Cart.length === 0 ? (
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjh0nc-PXNOFUmdVVUIrHTJlmiWyMxF9da5g&s" />
-      </div>
-        <h3>
-          O carrinho está <span style={{ "fontWeight": "bold" }}>vazio</span>
-        </h3>    
+      <h1>Sapoato</h1>
 
-  
-    <div>
-      <div onClick={goBack} className="cursor-pointer"><h2>Voltar</h2></div>
-      </div>
-
-      ) : ( 
       
-      <div>
-        {Cart.map(item =>
+      {cart.length === 0 ? (
+        <div>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjh0nc-PXNOFUmdVVUIrHTJlmiWyMxF9da5g&s" />
+          <h3>
+            O carrinho está <span style={{ "fontWeight": "bold" }}>vazio</span>
+          </h3>
+
+
+
+          <div onClick={goBack} className="cursor-pointer"><h2>Voltar</h2></div>
+        </div>
+
+      ) : (
+        cart.map(item =>
+        <div>
         
           
-
+        </div>
+      )
       
-      }
+      
+      )} 
 
-
-   <div className="cursor-pointer"> <footer>Finalizar compra</footer> </div>
-
-   <div className=" wd-widget">
-
-
-
-   </div>
-
-  
-
+      <div className="cursor-pointer"> 
+        <footer>Finalizar compra</footer> 
+      </div>
+    <div className="wd-widget"></div> 
+    </div>
   </div>
+  )
 }
 export default Cart
 
